558ec3ab3db2dd848a66c2a9f8165a36de786c7a
dev.deanie <dev.deanie@gmail.com>
2024-08-21 17:08:02 +0900

    테스트

--- composables/useCheckOs.ts
+++ composables/useCheckOs.ts
@@ -17,6 +17,9 @@ export const useCheckOs = () => {
         "https://apps.apple.com/kr/app/%EC%98%A4%EB%8B%A4-%EC%98%A4%EB%8A%98%EC%9D%98%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-%ED%95%9C%EC%9D%98%EC%82%AC-1-1-%EC%BC%80%EC%96%B4-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8%EC%9D%BC%EA%B8%B0/id1637382274"
       );
     } else {
+      return window.open(
+        "https://play.google.com/store/apps/details?id=kr.oda.flutter.app&pcampaignid=web_share"
+      );
     }
   };
 
